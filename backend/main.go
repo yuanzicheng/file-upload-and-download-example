89b114c40a88f6601668c7b481b097a44be16c7e
Aioria <11429050@qq.com>
2025-03-15 15:57:46 +0800

    upload

--- backend/main.go
+++ backend/main.go
@@ -1,112 +1,71 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
-	"io/fs"
+	"io"
 	"log"
 	"net/url"
 	"os"
-	"strconv"
 
-	"github.com/duke-git/lancet/v2/convertor"
 	"github.com/duke-git/lancet/v2/cryptor"
-	"github.com/duke-git/lancet/v2/slice"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/requestid"
 	"github.com/pkg/errors"
 )
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{EnablePrintRoutes: true, ErrorHandler: fiber.DefaultErrorHandler})
 
 	app.Use(requestid.New())
 
 	api := app.Group("/api")
 	api.Post("/upload", Upload)
 	api.Get("/download/:id", Download)
+	// api.Head("/download/:id", Download)
 
 	log.Fatal(app.Listen(fmt.Sprintf(":%v", 8080)))
 }
 
 func Upload(c *fiber.Ctx) error {
-	// formData, err := c.MultipartForm()
-	// if err != nil {
-	// 	return errors.WithStack(err)
-	// }
-	// fmt.Printf("%+v", formData)
+	// extracting `Content-Range` from request header
+	contentRange := c.Get("Content-Range")
+	fmt.Printf("%#v\n", contentRange)
 
-	index, _ := convertor.ToInt(c.FormValue("index"))
-	total, _ := convertor.ToInt(c.FormValue("total"))
-	name := c.FormValue("name")
-	size, _ := convertor.ToInt(c.FormValue("size"))
-	hash := c.FormValue("hash")
-	chunk, err := c.FormFile("file")
+	formData, err := c.MultipartForm()
 	if err != nil {
 		return errors.WithStack(err)
 	}
+	fmt.Printf("%#v\n", formData)
 
-	// Check file chunk size.
-	var chunkSize int64 = 1024 * 1024
-	if (index < total-1 && chunk.Size != chunkSize) || (index == (total-1) && chunk.Size != (size%chunkSize)) {
-		return errors.New("Invalid chunk size.")
-	}
-
-	// Create a directory named `hash` is the directory is not exist.
-	info, err := os.Stat(hash)
-	if (err != nil && os.IsNotExist(err)) || !info.IsDir() {
-		if err := os.Mkdir(hash, 0777); err != nil {
-			return errors.WithStack(err)
-		}
-	}
-
-	// TODO Check if this chunk is already uploaded.
-
-	// save file chunk to the directory named `hash`.
-	if err := c.SaveFile(chunk, fmt.Sprintf("%v/%v", hash, index)); err != nil {
+	fileHeader, err := c.FormFile("file")
+	if err != nil {
 		return errors.WithStack(err)
 	}
+	fmt.Printf("%#v\n", fileHeader)
 
-	// Check if all files have been uploaded, and complete the uploading, merge all the chunks into one file.
-	entries, err := os.ReadDir(hash)
-	if len(entries) >= int(total) {
-		completed := true
-		result := slice.Map(
-			slice.Filter(entries, func(_ int, entry fs.DirEntry) bool { return !entry.IsDir() }),
-			func(_ int, entry fs.DirEntry) string { return entry.Name() },
-		)
-		for i := 0; i < int(total-1); i++ {
-			if !slice.Contain(result, strconv.Itoa(i)) {
-				completed = false
-				break
-			}
-		}
-		if completed {
-			file, err := os.OpenFile(fmt.Sprintf("%v/%v", hash, name), os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-			defer file.Close()
-			writer := bufio.NewWriter(file)
-			for i := 0; i <= int(total-1); i++ {
-				filename := fmt.Sprintf("%v/%v", hash, i)
-				bytes, _ := os.ReadFile(filename)
-				writer.Write(bytes)
-				if err := os.Remove(filename); err != nil {
-					log.Println(err)
-				}
-				index++
-			}
-			writer.Flush()
-		}
+	f, _ := fileHeader.Open()
+	defer f.Close()
+	bytes, _ := io.ReadAll(f)
+	// 将字节数据追加写入指定文件
+
+	file, err := os.OpenFile("replace-with-your-file-path", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("failed to open the file: %v", err)
+	}
+	defer file.Close()
+
+	// 写入数据
+	_, err = file.Write(bytes)
+	if err != nil {
+		log.Fatalf("failed to write to the file: %v", err)
 	}
 
-	return c.JSON("ok")
+	return c.SendStatus(fiber.StatusOK)
 }
 
 func Download(c *fiber.Ctx) error {
-	const filename = "test.pdf"
+	const filename = "d:\\test.pptx"
 	f, err := os.Open(filename)
 	if err != nil {
 		return errors.WithStack(err)
@@ -128,6 +87,7 @@ func Download(c *fiber.Ctx) error {
 	start := rangeData.Ranges[0].Start
 	end := rangeData.Ranges[0].End
 	length := end - start + 1
+	fmt.Println(start, end, fileinfo.Size())
 	// TODO check chunk size
 
 	b := make([]byte, length)
